f67cdc98816174d3759e1a8e1cfbb756f6bc433b
Jose Alor <josea.alor@gmail.com>
2022-05-05 12:16:27 +0200

    logica juego principal casi completa

--- eventlistener.js
+++ eventlistener.js
@@ -1,6 +1,31 @@
 "use strict";
-const lettersArray = ["c", "a", "s", "a"];
-const guionesArray = ["_ ", "_ ", "_ ", "_ "];
+const palabras = [
+  "sanguijuela",
+  "electrocardiograma",
+  "cocodrilo",
+  "emparedado",
+  "lavadora",
+  "patata",
+  "camioneta",
+  "brazo",
+  "holograma",
+];
+
+const random = Math.floor(Math.random() * palabras.length);
+
+const secretWord = palabras[random]; //secretWord es la palabra random que tiene que aparecer en el texto
+
+const lettersArray = []; // ["m", "e", "s", "a"](esta no aparece en pantalla) [-,-,s,a](si aparece en pantalla) usserLetter ="a" contador(imagen)=1
+for (let i = 0; i < secretWord.length; i++) {
+  console.log(secretWord[i]);
+
+  lettersArray.push(secretWord[i]);
+}
+// Aqui generamos otro array con tantos guiones como lettersArray
+const guionesArray = [];
+for (let i = 0; i < secretWord.length; i++) {
+  guionesArray.push("_");
+}
 const main = document.querySelector("main");
 
 // Hay que juntar guionesArray en una string
@@ -31,11 +56,44 @@ form.addEventListener("submit", (event) => {
   if (!userGuessed) {
     errorCounter += 1;
   }
-
+  //Agregamos el nuevo contenido al p de la pantalla
   p.textContent = guionesArray.join("");
   console.log(errorCounter);
   console.log(guionesArray);
   //Quitamos el valor de la casilla una vez se envia la guessLetter
   form.elements.input.value = "";
   //Aqui se crean los p dependiendo de guionesArray
+
+  //Imagenes dependiendo de errores
+  function display_image(src, width, height, alt) {
+    let image = document.createElement("img");
+    image.src = src;
+    image.width = width;
+    image.height = height;
+    image.alt = alt;
+    document.body.appendChild(image);
+  }
+
+  if (errorCounter === 1) {
+    display_image("./imagenes/1.png", 250, 330, "primer error");
+  } else if (errorCounter === 2) {
+    display_image("./imagenes/2.png", 250, 330, "segundo error");
+  } else if (errorCounter === 3) {
+    display_image("./imagenes/3.png", 250, 330, "tercer error");
+  } else if (errorCounter === 4) {
+    display_image("./imagenes/4.png", 250, 330, "cuarto error");
+  } else if (errorCounter === 5) {
+    display_image("./imagenes/5.png", 250, 330, "quinto error");
+  } else if (errorCounter === 6) {
+    display_image("./imagenes/6.png", 250, 330, "Game Over");
+    errorCounter = 0;
+    //boton start
+  } else if (errorCounter <6 && //guionesArray completo )
+  ){ console.log("Has ganado, enhorabuena")
+  //boton start again
+
+  }
+  else {
+    display_image("solo el soporte");
+  }
 });
--- map.js
+++ map.js
@@ -1,8 +1,16 @@
 "use strict";
-const word = "casita";
-
-const splittedword = word.split("");
-console.log(splittedword);
-
-splittedword.replace("i", "_");
-console.log(word);
+//jugamos hasta que errores =6. Errores=6 (gameOver y reset)
+//jugamos hasta que la palabra sea completada...comprobar que guionesArray no tiene _
+//imagenes....
+const stopGame = () => {
+  if (errorCounter === 6) {
+    errorCounter = 0;
+    //boton start  y se reinicia el juego}
+  }
+  if (guionesArray[i] !== "_") {
+    console.log("Has ganado!");
+    errorCounter = 0;
+    //aparece boton start y se reinicia el juego
+  }
+};
+//botón start... (event Listener){ se reinicia el juego}
